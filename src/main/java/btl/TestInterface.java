c1b749d69eba2946f813907745c08106917a4036
Marcus Buffett <marcusbuffett@me.com>
2016-11-23 23:11:04 -0800

    Add TwoDigitAddition field to TestInterface class
    TODO: Make this take any class that implements the Strategy
    interface, once I have internet access to look up that
    syntax

--- src/main/java/btl/TestInterface.java
+++ src/main/java/btl/TestInterface.java
@@ -1,5 +1,8 @@
 public class TestInterface {
-  public TestInterface() {
+  TwoDigitAddition strategy;
+
+  public TestInterface(TwoDigitAddition strategy) {
+    this.strategy = strategy;
   }
   public Report runStrategy() {
     return new Report();
