75fe1838b84678f1d1a1aae720dc41cff84b46dc
Marcus Buffett <marcusbuffett@me.com>
2016-11-24 04:52:44 -0800

    Replace skeleton TwoDigitAddition with real impl

--- src/main/java/btl/strategies/TwoDigitAddition.java
+++ src/main/java/btl/strategies/TwoDigitAddition.java
@@ -4,13 +4,15 @@ import btl.Strategy;
 import btl.Question;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class TwoDigitAddition implements Strategy {
   public Question generateQuestion(long seed) {
-    // Get a random two digit number from seed
-    // Get a random two digit number from seed
-    // answer = a + b
-    // questionRep = Concat numbers together
-    return new Question("23 + 73", "96");
+    Random generator = new Random(seed);
+    int a = generator.nextInt(99) + 1;
+    int b = generator.nextInt(99) + 1;
+    String question = String.format("%d + %d", a, b);
+    String answer = Integer.toString(a + b);
+    return new Question(question, answer);
   }
 }
