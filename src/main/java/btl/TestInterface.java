58a799720c6b9f63d9e0b297772904d65e5c9d8c
Marcus Buffett <marcusbuffett@me.com>
2016-11-23 23:22:19 -0800

    Add skeleton code for TestInterface#askQuestion

--- src/main/java/btl/TestInterface.java
+++ src/main/java/btl/TestInterface.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+import btl.Question;
+
 public class TestInterface {
   TwoDigitAddition strategy;
+  Scanner scanner;
 
   public TestInterface(TwoDigitAddition strategy) {
     this.strategy = strategy;
+    this.scanner = new Scanner(System.in);
   }
+
   public Report runStrategy() {
     return new Report();
+  }
+  
+  public Answer askQuestion(Question question) {
+    System.out.println(question.questionRep);
+    String response = scanner.nextLine();
+    float time = 10.03f;
+    return new Answer(time, question, response);
   }
 }
