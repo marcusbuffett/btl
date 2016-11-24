9c4eeb202c201366ed715d841ff52d28b80e0462
Marcus Buffett <marcusbuffett@me.com>
2016-11-23 23:21:20 -0800

    Add constructor to Answer class

--- src/main/java/btl/Answer.java
+++ src/main/java/btl/Answer.java
@@ -5,6 +5,12 @@ public class Answer {
   Question question;
   String answer;
 
+  public Answer(float timeToAnswer, Question question, String answer) {
+    this.timeToAnswer = timeToAnswer;
+    this.question = question;
+    this.answer = answer;
+  }
+
   public boolean isCorrect() {
     return this.answer == this.question.correctAnswer;
   }
