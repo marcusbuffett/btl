30e6a616f61a56c33a883df7dd6cf6220ab4517c
Marcus Buffett <marcusbuffett@me.com>
2016-11-23 21:55:00 -0800

    Add constructor for Question

--- src/main/java/btl/Question.java
+++ src/main/java/btl/Question.java
@@ -5,4 +5,9 @@ import java.util.Optional;
 public class Question {
   public String questionRep;
   public Optional<String> correctAnswer;
+
+  public Question(String questionRep, Optional<String> correctAnswer) {
+    this.questionRep = questionRep;
+    this.correctAnswer = correctAnswer;
+  }
 }
