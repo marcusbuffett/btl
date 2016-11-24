1ce12eb9fe43f0469451a2ed18f0f2059dbce9b7
Marcus Buffett <marcusbuffett@me.com>
2016-11-24 04:55:04 -0800

    Run the TwoDigitAddition strategy

--- src/main/java/btl/Main.java
+++ src/main/java/btl/Main.java
@@ -1,9 +1,12 @@
 package btl;
 
 import btl.TestInterface;
+import btl.TwoDigitAddition;
 
 public class Main {
   public static void main(String[] args) {
     TestInterface testInterface = new TestInterface();
+    Report report = testInterface.runStrategy(new TwoDigitAddition());
+    System.out.print(report.generateReportRep());
   }
 }
