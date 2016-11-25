b5c67edc2c2eacf07f28b40223012a10d8aae2a5
Marcus Buffett <marcusbuffett@me.com>
2016-11-25 14:49:59 -0800

    Use menu interface

--- src/main/java/btl/Main.java
+++ src/main/java/btl/Main.java
@@ -1,12 +1,10 @@
 package btl;
 
-import btl.TestInterface;
-import btl.NDigitAddition;
+import btl.MenuInterface;
 
 public class Main {
   public static void main(String[] args) {
-    TestInterface testInterface = new TestInterface();
-    Report report = testInterface.runStrategy(new NDigitAddition(3));
-    System.out.print(report.generateReportRep());
+    MenuInterface menuInterface = new MenuInterface();
+    menuInterface.start();
   }
 }
