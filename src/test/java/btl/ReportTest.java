eeea8e065c1f7673aa8d7eb833745a1d4113479a
Marcus Buffett <marcusbuffett@me.com>
2016-11-27 03:35:22 -0800

    Add test for Report#generateStatsRep method

--- src/test/java/btl/ReportTest.java
+++ src/test/java/btl/ReportTest.java
@@ -2,7 +2,9 @@ package btl;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class ReportTest {
   @Test
@@ -17,5 +19,23 @@ public class ReportTest {
       .append("Time taken: " + answer.timeToAnswer + "\n")
       .toString();
     assertEquals(expectedAnswer, report.generateAnswerRep(answer));
+  }
+
+  @Test
+  public void testGenerateStatsRep() {
+    List<Answer> answers = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      Question question = new Question("x + y", new Integer(i).toString());
+      // Half correct, half incorrect
+      String response = new Integer(i % 2 == 0 ? i : -1).toString();
+      Answer answer = new Answer((float) i, question, response);
+      answers.add(answer);
+    }
+    Report report = new Report(answers, ReportFormat.LONG);
+    String expectedAnswer = new StringBuilder()
+      .append("Score: 5 / 10\n")
+      .append("Average time: 4.50s\n")
+      .toString();
+    assertEquals(expectedAnswer, report.generateStatsRep());
   }
 }
