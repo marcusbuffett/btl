87477533dd207329ffe8e3a50c140384de9f581c
Marcus Buffett <marcusbuffett@me.com>
2016-11-26 19:30:03 -0800

    Add test for Complement strategy

---
+++ src/test/java/btl/strategies/ComplementTest.java
@@ -0,0 +1,18 @@
+package btl;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import btl.*;
+
+public class ComplementTest {
+  @Test
+  public void testGenerateQuestion() {
+    Complement complementStrategy = new Complement();
+    for (long i = 0; i < 10; i++) {
+      Question question = complementStrategy.generateQuestion(i);
+      int x = Integer.parseInt(question.questionRep);
+      int complement = 100 - x;
+      assertEquals(Integer.parseInt(question.correctAnswer), complement);
+    }
+  }
+}
